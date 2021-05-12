66663bb765a201c4f6348b78b918c59dd8c55a35
Mitchell <Mitchell@Family-Mac-mini-2.local>
2021-05-12 15:04:10 -0500

    added additional console.log to correct grammar in song when going from 2 lines to 1 line.

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ let friends = [`Darren`, `Rachel`, `Zoe`, `Kenzie`, `Emily Mountain`];
 
 // Function to Sing the "99 lines of Code" song.  Function takes the argument of the name of the person singing.  
 // Each line of code will use String interpolation to correctly display how many lines of code there are currently (based off the index) and who is singing the song.
-// The last line of the song is written out seperately due to "line" being singular.
+// The last two lines of the song are written out seperately due to "line" being singular.
 function song(name) {
-    for (let i = 99; i > 1; i--) {
+    for (let i = 99; i > 2; i--) {
         console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file.`)
     };
+    console.log(`2 lines of code in the file, 2 lines of code; ${name} strikes one out, clears it all out, 1 line of code in the file.`)
     console.log(`1 line of code in the file, 1 line of code; ${name} strikes one out, clears it all out, 0 lines of code in the file.`)
 };
 
